f9584f6c5def863059f663d85a3e88aaa4d51e2e
Siqueira93 <raul.soaressiqueira@gmail.com>
2024-10-16 11:44:37 -0300

    .

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -10,4 +10,40 @@ public class SmartTv {
    int canal = 1;
    int volume = 25;
    String marca = "SONY";
+
+
+   /* 2. Nossa TV poderá ligar e desligar e assim mudar o estado ligada */
+   public void ligar(){
+      ligada = true;
+      //Aqui confirmo que a TV está ligada igual a TRUE
+   }
+   public void desligar(){
+      ligada = false;
+      //Aqui confirmo que a TV está desligada igual a FALSE
+   }
+
+   /* 3. Nossa TV aumentará e diminuirá o volume sempre em +1 ou -1 */
+   public void aumentarVolume(){
+     //volume = volume + 1; 
+      volume++; 
+   }
+   public void diminuirVolume(){
+     //volume = volume - 1; 
+      volume--; 
+   }
+
+   /* 4. Nossa TV poderá mudar de canal de 1 em 1 ou definindo o número correspondente */
+   public void mudarCanal( int novoCanal){
+      canal = novoCanal;
+   }
+   public void proximoCanal(){
+     //volume = volume + 1; 
+      canal++; 
+   }
+   public void voltarCanal(){
+     //volume = volume - 1; 
+      canal--; 
+   }
+
+
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,8 +1,49 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
         
-        SmartTv smartv = new SmartTv();
+        SmartTv smartv = new SmartTv(); //Orientação a Objetos;
 
+        System.out.println("TV LIGADA ? " + smartv.ligada);
+        System.out.println("Canal Atual : " + smartv.canal);
+        System.out.println("Volume total ? " + smartv.volume);
+        
+        // INTERAÇÃO COM O USARIO
+        smartv.ligar(); // usuário aperta no BOTÃO LIGAR
+        System.out.println("Novo Status -> TV LIGADA ? " + smartv.ligada); 
+        
+        smartv.desligar(); // usuário aperta no BOTÃO DESLIGAR
+        System.out.println("Novo Status -> TV DESLIGADA ? " + smartv.ligada);
+
+        //PRÓXIMO
+        smartv.aumentarVolume(); // usuário aperta no BOTÃO VOLUME +
+        System.out.println("Aumentou o volume: " + smartv.volume); // 26
+    
+
+        smartv.diminuirVolume(); // usuário aperta no BOTÃO VOLUME - 
+        System.out.println("Diminuiu o volume: " + smartv.volume); // 25
+        smartv.diminuirVolume(); // usuário aperta no BOTÃO VOLUME - 
+        System.out.println("Diminuiu o volume: " + smartv.volume); // 24
+        smartv.diminuirVolume(); // usuário aperta no BOTÃO VOLUME - 
+        System.out.println("Diminuiu o volume: " + smartv.volume); // 23
+
+        //PRÓXIMO
+        smartv.proximoCanal(); // usuário aperta no BOTÃO VOLUME +
+        System.out.println("Mudou para o canal: " + smartv.canal); // 2
+        smartv.proximoCanal(); // usuário aperta no BOTÃO VOLUME +
+        System.out.println("Mudou para o canal: " + smartv.canal); // 3
+        smartv.proximoCanal(); // usuário aperta no BOTÃO VOLUME +
+        System.out.println("Mudou para o canal: " + smartv.canal); // 4
+        smartv.proximoCanal(); // usuário aperta no BOTÃO VOLUME +
+        System.out.println("Mudou para o canal: " + smartv.canal); // 5
+    
+
+        smartv.voltarCanal(); // usuário aperta no BOTÃO VOLUME - 
+        System.out.println("Mudou para o canal: " + smartv.canal); // 4
+
+        smartv.mudarCanal(2);
+        System.out.println("Canal atual é: " + smartv.canal); //2
+
+        
         
     }
 }
