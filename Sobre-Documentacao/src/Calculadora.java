30e5b23b3d7e05c4d7df6cf9734a9a24e0a5308e
Siqueira93 <raul.soaressiqueira@gmail.com>
2024-10-16 13:00:12 -0300

    .

---
+++ Sobre-Documentacao/src/Calculadora.java
@@ -0,0 +1,25 @@
+/**
+ * <h1>CALCULADORA</h1>
+ * <p> A Calculadora realiza operações matemáticas entre numeros inteiros</p>
+ * <b> Note:</b> Leia atentamente a documentação desta classes para desfrutar dos recusos oferecidos pelo autor
+ * 
+ * @author Raul Siqueira
+ * @version 1.0
+ * @since 01/01/2024
+ */
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois numeros inteiros
+     * @param numeroUm este é o primeiro parametro
+     * @param numeroDois este é o segundo parametro 
+     * @return int o resultado deste metodo é soma dos dois numeros
+     */
+
+     public int somar(int numeroUm, int numeroDois ){
+        return numeroUm + numeroDois;
+    
+     }
+
+    
+    
+}
---
+++ Sobre-Documentacao/src/Comentarios.java
@@ -0,0 +1,26 @@
+public class Comentarios {
+    public static void main(String[] args) {
+            // Olá, eu sou um comentario de linha unica
+
+            /*
+            * Olá,
+            * eu sou um comentario maior
+            * mais detalhado
+            */
+    }
+
+
+        /**
+         * Está duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentario
+         * a nivel de documentacao
+         * Que incrivel!!!
+         */
+    public void metodo(){
+
+    }   
+
+         
+   
+}
