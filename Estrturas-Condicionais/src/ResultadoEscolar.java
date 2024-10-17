5dc7eac4afbe2a80992c321bf3c7984944351b21
RaulSSiqueira <raul.soaressiqueira@gmail.com>
2024-10-17 20:17:35 -0300

    .

---
+++ Estrturas-Condicionais/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+public class ResultadoEscolar {
+    // CONDICIONAL COMPOSTA  QUANDO A DOIS OU MAIS RESULTADOS POSSIVEIS
+
+    public static void main(String[] args) {
+        int nota = 6;
+
+            if (nota >= 7) {
+                System.out.println("Aprovado");
+                System.out.println("");
+            }
+
+            else
+                System.out.println("Reprovado");
+        
+    }
+    
+}
